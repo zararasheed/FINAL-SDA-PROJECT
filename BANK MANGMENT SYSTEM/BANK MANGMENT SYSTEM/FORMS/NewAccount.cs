55a39d4f3c28bdfb353d1cfd6954ecff6863081c
Your Name <you@example.com>
2020-06-09 00:27:15 +0500

    newaccount form creation

--- BANK MANGMENT SYSTEM/BANK MANGMENT SYSTEM/FORMS/NewAccount.cs
+++ BANK MANGMENT SYSTEM/BANK MANGMENT SYSTEM/FORMS/NewAccount.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,43 @@ namespace BANK_MANGMENT_SYSTEM.FORMS
 {
     public partial class NewAccount : Form
     {
+        string gender = string.Empty;
+        string m_status = string.Empty;
+        decimal no;
+
+        Bank_Mangment_SystemEntities1 BSE;
+        MemoryStream ms;
+        
         public NewAccount()
         {
             InitializeComponent();
+            loaddate();
+            loadaccount();
+            loadstate();
+
+
+
+        }
+
+        private void loadstate()
+        {
+            throw new NotImplementedException();
+        }
+        private void loadaccount()
+        {
+            BSE = new Bank_Mangment_SystemEntities1();
+            
+
+        }
+
+        private void loaddate()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void NewAccount_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
