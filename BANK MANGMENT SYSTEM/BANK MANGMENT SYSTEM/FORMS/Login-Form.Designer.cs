6cb26f51f2151e88eba254b0e0d44f067f187f3d
Your Name <you@example.com>
2020-06-08 22:55:14 +0500

    admin login form fully created

--- BANK MANGMENT SYSTEM/BANK MANGMENT SYSTEM/FORMS/Login-Form.Designer.cs
+++ BANK MANGMENT SYSTEM/BANK MANGMENT SYSTEM/FORMS/Login-Form.Designer.cs
@@ -66,6 +66,7 @@
             this.button1.TabIndex = 2;
             this.button1.Text = "LOGIN";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label3
             // 
--- BANK MANGMENT SYSTEM/BANK MANGMENT SYSTEM/FORMS/Login-Form.cs
+++ BANK MANGMENT SYSTEM/BANK MANGMENT SYSTEM/FORMS/Login-Form.cs
@@ -16,5 +16,32 @@ namespace BANK_MANGMENT_SYSTEM.FORMS
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bank_Mangment_SystemEntities1 dbe = new Bank_Mangment_SystemEntities1();
+            if (usertextBox.Text!= String.Empty || passwordtextBox.Text!= String.Empty)
+            {
+                var user1 = dbe.Admin_Table.FirstOrDefault(a => a.Username.Equals(usertextBox.Text));
+                if (user1 != null)
+                {
+                    if (user1.Password.Equals(passwordtextBox.Text))
+                    {
+
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Null values")
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter username or password","Error",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+            }
+
+
+        }
     }
 }
