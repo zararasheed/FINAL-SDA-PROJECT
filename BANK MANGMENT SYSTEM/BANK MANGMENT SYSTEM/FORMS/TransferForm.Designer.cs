f4135280df73c5e9dd494b8e5573b64323d98f4c
Your Name <you@example.com>
2020-06-10 04:55:18 +0500

    transfer form successfully completed

--- BANK MANGMENT SYSTEM/BANK MANGMENT SYSTEM/FORMS/TransferForm.Designer.cs
+++ BANK MANGMENT SYSTEM/BANK MANGMENT SYSTEM/FORMS/TransferForm.Designer.cs
@@ -28,12 +28,182 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.datelbl = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.fromacctxt = new System.Windows.Forms.TextBox();
+            this.nametxt = new System.Windows.Forms.TextBox();
+            this.amounttxt = new System.Windows.Forms.TextBox();
+            this.desacctxt = new System.Windows.Forms.TextBox();
+            this.transfertxt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(279, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Date";
+            // 
+            // datelbl
+            // 
+            this.datelbl.AutoSize = true;
+            this.datelbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.datelbl.Location = new System.Drawing.Point(334, 64);
+            this.datelbl.Name = "datelbl";
+            this.datelbl.Size = new System.Drawing.Size(0, 17);
+            this.datelbl.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "From Account No";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Current Amount";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 239);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Destination Account";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(31, 286);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Amount";
+            // 
+            // fromacctxt
+            // 
+            this.fromacctxt.Location = new System.Drawing.Point(161, 107);
+            this.fromacctxt.Name = "fromacctxt";
+            this.fromacctxt.Size = new System.Drawing.Size(185, 20);
+            this.fromacctxt.TabIndex = 7;
+            // 
+            // nametxt
+            // 
+            this.nametxt.Location = new System.Drawing.Point(161, 142);
+            this.nametxt.Name = "nametxt";
+            this.nametxt.Size = new System.Drawing.Size(185, 20);
+            this.nametxt.TabIndex = 8;
+            // 
+            // amounttxt
+            // 
+            this.amounttxt.Location = new System.Drawing.Point(161, 180);
+            this.amounttxt.Name = "amounttxt";
+            this.amounttxt.Size = new System.Drawing.Size(185, 20);
+            this.amounttxt.TabIndex = 9;
+            // 
+            // desacctxt
+            // 
+            this.desacctxt.Location = new System.Drawing.Point(161, 233);
+            this.desacctxt.Name = "desacctxt";
+            this.desacctxt.Size = new System.Drawing.Size(185, 20);
+            this.desacctxt.TabIndex = 10;
+            // 
+            // transfertxt
+            // 
+            this.transfertxt.Location = new System.Drawing.Point(161, 279);
+            this.transfertxt.Name = "transfertxt";
+            this.transfertxt.Size = new System.Drawing.Size(185, 20);
+            this.transfertxt.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(390, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 54);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "SHOW DETAILS";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(199, 329);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 43);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "TRANSFER ACCOUNT";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // TransferForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.transfertxt);
+            this.Controls.Add(this.desacctxt);
+            this.Controls.Add(this.amounttxt);
+            this.Controls.Add(this.nametxt);
+            this.Controls.Add(this.fromacctxt);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.datelbl);
+            this.Controls.Add(this.label1);
+            this.Name = "TransferForm";
             this.Text = "TransferForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label datelbl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox fromacctxt;
+        private System.Windows.Forms.TextBox nametxt;
+        private System.Windows.Forms.TextBox amounttxt;
+        private System.Windows.Forms.TextBox desacctxt;
+        private System.Windows.Forms.TextBox transfertxt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- BANK MANGMENT SYSTEM/BANK MANGMENT SYSTEM/FORMS/TransferForm.cs
+++ BANK MANGMENT SYSTEM/BANK MANGMENT SYSTEM/FORMS/TransferForm.cs
@@ -15,6 +15,53 @@ namespace BANK_MANGMENT_SYSTEM.FORMS
         public TransferForm()
         {
             InitializeComponent();
+            loaddate();
+        }
+
+        private void loaddate()
+        {
+            datelbl.Text = DateTime.UtcNow.ToString("MM/dd/yyyy");
+            //throw new NotImplementedException();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bank_Mangment_SystemEntities1 dbe = new Bank_Mangment_SystemEntities1();
+            decimal b = Convert.ToDecimal(fromacctxt.Text);
+            var item = (from u in dbe.userAccounts where u.Account_No == b select u).FirstOrDefault();
+            nametxt.Text = item.Name;
+            amounttxt.Text = Convert.ToString(item.balance);
+         
+      }
+    
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bank_Mangment_SystemEntities1 dbe = new Bank_Mangment_SystemEntities1();
+            decimal b = Convert.ToDecimal(fromacctxt.Text);
+            var item = (from u in dbe.userAccounts where u.Account_No == b select u).FirstOrDefault();
+            decimal b1 = Convert.ToDecimal(item.balance);
+            decimal totalbal = Convert.ToDecimal(transfertxt.Text);
+            decimal transferacc = Convert.ToDecimal(desacctxt.Text);
+            if (b1>totalbal)
+            {
+                userAccount item2 = (from u in dbe.userAccounts where u.Account_No == transferacc select u).FirstOrDefault();
+                item2.balance = item2.balance + totalbal;
+                item.balance = item.balance -totalbal;
+
+                Transfer transfer = new Transfer(); 
+                  transfer.Account_No=  Convert.ToDecimal(fromacctxt.Text);
+                transfer.Name = nametxt.Text;
+                transfer.ToTransfer = Convert.ToDecimal(desacctxt.Text);
+                transfer.Date = DateTime.UtcNow.ToString();
+                transfer.balance = Convert.ToDecimal(transfertxt.Text);
+
+                dbe.Transfers.Add(transfer);
+                dbe.SaveChanges();
+                MessageBox.Show("Transfer Money Successfully");
+
+            }
+
+
         }
     }
 }
