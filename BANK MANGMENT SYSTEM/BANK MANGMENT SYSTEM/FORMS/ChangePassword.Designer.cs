e141a928a9faa63b42c5fe89d4a2067b208d7078
asadrao91359 <asadrao91359@gmail.com>
2020-06-15 02:43:19 +0500

    ChangePassword form completed

--- BANK MANGMENT SYSTEM/BANK MANGMENT SYSTEM/FORMS/ChangePassword.Designer.cs
+++ BANK MANGMENT SYSTEM/BANK MANGMENT SYSTEM/FORMS/ChangePassword.Designer.cs
@@ -29,20 +29,136 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChangePassword));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.oldpasswordtxt = new System.Windows.Forms.TextBox();
+            this.newpasswordtxt = new System.Windows.Forms.TextBox();
+            this.retypepasswordtxt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.usertxt = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(192, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(274, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Change Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Old Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(42, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "New Password";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(42, 215);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Re-type Password";
+            // 
+            // oldpasswordtxt
+            // 
+            this.oldpasswordtxt.Location = new System.Drawing.Point(178, 126);
+            this.oldpasswordtxt.Name = "oldpasswordtxt";
+            this.oldpasswordtxt.Size = new System.Drawing.Size(215, 20);
+            this.oldpasswordtxt.TabIndex = 6;
+            // 
+            // newpasswordtxt
+            // 
+            this.newpasswordtxt.Location = new System.Drawing.Point(178, 168);
+            this.newpasswordtxt.Name = "newpasswordtxt";
+            this.newpasswordtxt.Size = new System.Drawing.Size(215, 20);
+            this.newpasswordtxt.TabIndex = 7;
+            // 
+            // retypepasswordtxt
+            // 
+            this.retypepasswordtxt.Location = new System.Drawing.Point(178, 207);
+            this.retypepasswordtxt.Name = "retypepasswordtxt";
+            this.retypepasswordtxt.Size = new System.Drawing.Size(215, 20);
+            this.retypepasswordtxt.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(178, 253);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 60);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Change";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "User Name";
+            // 
+            // usertxt
+            // 
+            this.usertxt.Location = new System.Drawing.Point(178, 88);
+            this.usertxt.Name = "usertxt";
+            this.usertxt.Size = new System.Drawing.Size(215, 20);
+            this.usertxt.TabIndex = 5;
             // 
             // ChangePassword
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.retypepasswordtxt);
+            this.Controls.Add(this.newpasswordtxt);
+            this.Controls.Add(this.oldpasswordtxt);
+            this.Controls.Add(this.usertxt);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "ChangePassword";
             this.Text = "ChangePassword";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox oldpasswordtxt;
+        private System.Windows.Forms.TextBox newpasswordtxt;
+        private System.Windows.Forms.TextBox retypepasswordtxt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox usertxt;
     }
 }
--- BANK MANGMENT SYSTEM/BANK MANGMENT SYSTEM/FORMS/ChangePassword.cs
+++ BANK MANGMENT SYSTEM/BANK MANGMENT SYSTEM/FORMS/ChangePassword.cs
@@ -16,6 +16,32 @@ namespace BANK_MANGMENT_SYSTEM.FORMS
         {
             InitializeComponent();
         }
-     
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bank_Mangment_SystemEntities1 dbe = new Bank_Mangment_SystemEntities1();
+            if (oldpasswordtxt.Text != string.Empty || newpasswordtxt.Text !=string.Empty || retypepasswordtxt.Text != string.Empty)
+             
+            {
+                Admin_Table user1 = dbe.Admin_Table.FirstOrDefault(a => a.Username.Equals(usertxt.Text));
+                if (user1 != null)
+                {
+                    if (user1.Password.Equals(oldpasswordtxt.Text))
+                    {
+                        user1.Password = newpasswordtxt.Text;
+                        dbe.SaveChanges();
+                        MessageBox.Show("Password Changed Successfully!");
+                    }
+                    else
+                    {
+
+                        MessageBox.Show("Password Incorrect");
+                    }
+                }
+
+
+
+            }
+        }
     }
 }
